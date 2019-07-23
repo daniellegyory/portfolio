136c9b630e5ca85548e7bc3aaf88e4ce79c7e23d
Danielle Gyory <daniellegyory@gmail.com>
2019-07-23 14:31:33 -0400

    Updates

---
+++ precache-manifest.a4d97c5b7fe66002934c4431a85b36a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09b391a1cbd4056b1296d06fe32996f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5d5aff0ba6d5d212d315",
+    "url": "/portfolio/static/css/main.b566f6cc.chunk.css"
+  },
+  {
+    "revision": "fd3731e156322d6c16f2",
+    "url": "/portfolio/static/js/2.985afc78.chunk.js"
+  },
+  {
+    "revision": "5d5aff0ba6d5d212d315",
+    "url": "/portfolio/static/js/main.e5f59ee3.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
