5cb5feed0622b754ce75533cf14e48d53d1f4f8e
Danielle Gyory <daniellegyory@gmail.com>
2019-07-23 16:27:40 -0400

    spell check

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default class About extends Component {
 				<h3 className="headline scroll-animated">About Me</h3>
 				<p className="scroll-animated">In May I graduated from Lehigh University with a B.S. in Computer Engineering and a concentration in Graphic Design.</p>
 				<h3 className="headline scroll-animated">What I Have Done</h3>
-				<p className="scroll-animated">This website! I coded this to get more practive with HTML, CSS, JavaScript, and React. I have experience in data animation with d3.js, prototyping apps with InVision and Adobe XD, and making visual artwork with Illustrator and Photoshop.</p>
+				<p className="scroll-animated">This website! I coded this to get more practice with HTML, CSS, JavaScript, and React. I have experience in data animation with d3.js, prototyping apps with InVision and Adobe XD, and making visual artwork with Illustrator and Photoshop.</p>
 				<h3 className="headline scroll-animated">What I Am Looking For</h3>
 				<p className="scroll-animated">I am seeking experiences in front-end, interactive, and UI design. I am actively searching for a collaborative team of designers to join in New York City. I love design-thinking and building with other people.</p>
 
